079dae27027e0b9eb195b6c2f6035d9d1480cf46
Carl Tsai <Carl Tsai>
2016-11-03 09:30:29 -0700

    added myGroupsTab template/controller

---
+++ www/js/controllers/myGroupsController.js
@@ -0,0 +1,40 @@
+angular.module('app.homeCtrlController', 
+    ['ionic', 'app.services', 'jett.ionic.filter.bar', 'app.userDatabaseService'])
+
+    .controller('myGroupsCtrl', ['$scope', 
+        '$stateParams', 
+        '$state', '$ionicLoading', 'userCourseGroupService',
+        function ($scope, 
+            $stateParams, $state, $ionicLoading, 
+            userCourseGroupService) {
+
+            function getItems () {
+                $scope.items = [];
+                $ionicLoading.show({
+                    template: 'Loading',
+                    delay: 50
+                });
+                userCourseGroupService.getUserGroups()
+                    .then(function(res){
+                        $scope.items = [];
+                        res.forEach(function(t){
+                            $scope.items.push({
+                                name: t.name;
+                            })
+                        })
+                        $ionicLoading.hide();
+                    }).catch(function(error){
+                        console.log("error !" + error);
+                        $ionicLoading.hide();
+                    });
+            }
+
+            getItems();
+
+            if ($stateParams.updateRequired) {
+                // force refresh
+                console.log("update is required");
+                getItems();
+            }
+        }])
+
