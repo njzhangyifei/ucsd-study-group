51e6d6f17bab61c9ef7ca7f288e27484970ecb28
Grace <graceferd17@gmail.com>
2016-11-03 18:27:26 -0700

    changed new study group controller

--- www/js/controllers/newStudyGroupController.js
+++ www/js/controllers/newStudyGroupController.js
@@ -2,31 +2,28 @@ angular.module('app.newStudyGroupController',
     ['ionic', 'app.services', 'jett.ionic.filter.bar', 'app.userDatabaseService'])
 
     .controller('newStudyGroupCtrl',
-        ['$scope', '$stateParams', 'groupDatabaseService',
+        ['$scope', '$stateParams', 'groupDatabaseService', 'courseDatabaseService',
             // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
             // You can include any angular dependencies as parameters for this function
             // TIP: Access Route Parameters for your page via $stateParams.parameterName
-            function ($scope, $stateParams) {
+            function ($scope, $stateParams, groupDatabaseService, courseDatabaseService) {
                 console.log("new study group for " + $stateParams.course.name);
 
                 $scope.createGroup = function(){
                     $scope.groupForm = {};
-                    $scope.createGroup = function(){
                         var group = {};
-                       //var hasName = false;
-                       // if($scope.groupForm.name) {
-                       //  hasName = true;
-                       // } else {
-                       //   return "Please enter a name for your group"
-                       // }
+                        var hasName = false;
+                        if($scope.groupForm.name) {
+                            hasName = true;
+                        } else {
+                            return "Please enter a name for your group";
+                        }
                         group.name = $scope.groupForm.name;
                         group.description = $scope.groupForm.description;
-                        
-                        courseDatabaseService.addGroup(group);
-                        //add creator?
-                        // TODO need to link course to the group
-                        // groupDatabaseService.createGroup(group);
+                        var cid = $stateParams.course.id;
+                        groupDatabaseService.createGroup(group);
+                        //needs gid
+                        return courseDatabaseService.addGroup(gid, cid);
                     }
-                }
             }])
-;
+
