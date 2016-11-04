159a82168c3d1dc231077500798ad081ee386f61
Yifei Zhang <njzhangyifei@gmail.com>
2016-11-03 19:18:39 -0700

    changed newstudygroup ctrl

--- www/js/controllers/newStudyGroupController.js
+++ www/js/controllers/newStudyGroupController.js
@@ -7,21 +7,26 @@ angular.module('app.newStudyGroupController',
             // You can include any angular dependencies as parameters for this function
             // TIP: Access Route Parameters for your page via $stateParams.parameterName
             function ($scope, $stateParams) {
-                console.log("new study group for " + $stateParams.course.name);
+                console.log("new study group for " + $stateParams.course.department + $stateParams.course.number);
 
                 $scope.createGroup = function(){
                     $scope.groupForm = {};
                     $scope.createGroup = function(){
                         var group = {};
-                       //var hasName = false;
-                       // if($scope.groupForm.name) {
-                       //  hasName = true;
-                       // } else {
-                       //   return "Please enter a name for your group"
-                       // }
+                        //var hasName = false;
+                        // if($scope.groupForm.name) {
+                        //  hasName = true;
+                        // } else {
+                        //   return "Please enter a name for your group"
+                        // }
                         group.name = $scope.groupForm.name;
                         group.description = $scope.groupForm.description;
-                        
+
+                        var groupRef = groupDatabaseService.createGroup(group);
+                        groupRef.then(function(){
+                            var k = groupRef.key();
+                            console.log(k);
+                        });
                         courseDatabaseService.addGroup(group);
                         //add creator?
                         // TODO need to link course to the group
