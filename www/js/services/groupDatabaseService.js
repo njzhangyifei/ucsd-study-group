917023838f90466e7c1a858183178897470367ea
Carl Tsai <dauthdaert@gmail.com>
2016-11-21 04:44:07 -0800

    added message read/write to groupDatabaseService

--- www/js/services/groupDatabaseService.js
+++ www/js/services/groupDatabaseService.js
@@ -4,13 +4,13 @@ angular.module('app.groupDatabaseService', ['ionic'])
             var db = firebase.database();
             var groupsPath = 'groups/';
             var membersPath = 'members/';
-            var usersPath = 'users/'
+            var usersPath = 'users/';
+            var messagePath = 'messages/'
 
             return {
                 createGroup: function(group){
                     // still needs work
-                    var currentUser = firebase.auth().currentUser.uid;
-                    group.creator = currentUser
+                    group.creator = userDatabaseService.getCurrentUserId()
                     var groupInfoRef = db.ref(groupsPath).push(group);
                     return groupInfoRef;
                 },
@@ -30,6 +30,22 @@ angular.module('app.groupDatabaseService', ['ionic'])
                     delete g.id;
                     delete g.member;
                     return groupInfoRef.update(g);
+                },
+                
+                writeMessage: function(groupId, content){
+                    var messageRef = db.ref(groupsPath + groupId + '/' + 
+                                            messagePath + new Date().now());
+                    var message = {
+                        user: userDatabaseService.getCurrentUserId(),
+                        content: content
+                    };
+                },
+                
+                getMessages: function(groupId, lim){
+                    var messageQuery = db.ref(groupsPath + groupId + '/' + messagePath).limitToLast(lim);
+                    return messageQuery.once('value').then(function(snapshot){
+                       return snapshot.val(); 
+                    });
                 }
             }
         }])
