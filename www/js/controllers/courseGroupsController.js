6a4a47c553845186087211d1b7e171e2917e26a6
Yifei Zhang <njzhangyifei@gmail.com>
2016-11-29 11:31:55 -0800

    added comments

--- www/js/controllers/courseGroupsController.js
+++ www/js/controllers/courseGroupsController.js
@@ -1,56 +1,72 @@
+// module injection
 angular.module('app.courseGroupsController',
     ['ionic',
      'app.services', 'app.courseDatabaseService', 'app.stateParamsService',
      'jett.ionic.filter.bar'])
 
+    // dependency injection
     .controller('courseGroupsCtrl',
         ['$scope', '$state', '$ionicLoading', 'courseDatabaseService', 'stateParamsService',
         function ($scope, $state, $ionicLoading, courseDatabaseService, stateParamsService) {
+                // update the state params from the service
                 $stateParams = stateParamsService.getStateParams();
+                // update the object binded
                 $scope.course = $stateParams.course;
 
+                // function for getting the course infos from the database
                 function getItems () {
                     $scope.items = [];
                     $ionicLoading.show({
                         template: 'Loading',
                         delay: 50
                     });
+                    // load the course
                     return courseDatabaseService.getGroups($stateParams.course.id)
                         .then(function(res){
+                            // load the course info async
                             $scope.items = [];
                             res.forEach(function(t){
                                 $scope.items.push(t)
                             })
                             $ionicLoading.hide();
                         }).catch(function(error){
-                            console.log("error !" + error);
+                            console.log('error !' + error);
                             $ionicLoading.hide();
                         });
                 }
 
+                // pull down to refresh
                 $scope.refreshItems = function () {
                     getItems().then(function(){
+                        // async callback
                         $scope.$broadcast('scroll.refreshComplete');
                     });
                 };
 
+                // selected item callback
                 $scope.selectedItem = function(group){
+                    // update the state param
                     stateParamsService.setStateParams('tabsController.groupDetail', {
                         group: group
                     })
+                    // transition
                     $state.go('tabsController.groupDetail')
                 }
 
+                // add a new group callback
                 $scope.addGroup = function() {
+                    // set stateparam for next view
                     stateParamsService.setStateParams('tabsController.newStudyGroup', {
                         course: $stateParams.course
                     })
+                    // transition
                     $state.go('tabsController.newStudyGroup');
                 }
 
+                // update is required
                 if ($stateParams.updateRequired) {
                     // force refresh
-                    console.log("update is required");
+                    console.log('update is required');
                     getItems();
                 }
             }])
--- www/js/controllers/homeController.js
+++ www/js/controllers/homeController.js
@@ -1,44 +1,56 @@
+// module injection
 angular.module('app.homeController',
     ['ionic', 'app.services', 'jett.ionic.filter.bar', 
         'app.userDatabaseService', 'app.stateParamsService'])
 
+    // dependency injection
     .controller('homeCtrl', ['$scope', '$stateParams',
         '$state', '$ionicLoading', '$ionicHistory', 'userCourseGroupService', 'stateParamsService',
         function ($scope,
             $stateParams, $state, $ionicLoading, $ionicHistory,
             userCourseGroupService, stateParamsService) {
+
+            // function for loading the courses of the user
             function getItems () {
                 $scope.items = [];
                 $ionicLoading.show({
                     template: 'Loading',
                     delay: 50
                 });
+                // async call for loading the user's coures
                 userCourseGroupService.getUserCourses()
                     .then(function(res){
+                        // get course info from key
                         $scope.items = [];
                         res.forEach(function(t){
                             $scope.items.push(t)
                         })
                         $ionicLoading.hide();
                     }).catch(function(error){
-                        console.log("error !" + error);
+                        console.log('error !' + error);
                         $ionicLoading.hide();
                     });
             }
 
+            // update is required
             if ($stateParams.updateRequired) {
                 // force refresh
-                console.log("update is required");
+                console.log('update is required');
                 getItems();
             }
 
+            // add course controller
             $scope.addCourse = function() {
+                // state transition
                 $state.go('tabsController.addCourse');
             }
 
+            // course selected callback
             $scope.selectedItem = function(item){
+                // update the state param for the course group
                 stateParamsService.setStateParams('tabsController.courseGroups', 
                     {course: item, updateRequired:true});
+                // state transition
                 $state.go('tabsController.courseGroups');
             }
         }])
